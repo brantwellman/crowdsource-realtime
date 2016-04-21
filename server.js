06a1bad5e60f976d8ea99a81f4e79af29497d5d1
Brant Wellman <BrantWellman@gmail.com>
2016-04-20 19:58:38 -0600

    Setup mocha and server-test file

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-
-app.use(express.static('public'));
-app.get('/', function (req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
-
 const port = process.env.PORT || 3000;
 const server = http.createServer(app)
                  .listen(port, function () {
                   console.log('Listenting on port ' + port + '.');
                  });
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res){
+  res.render('index');
+});
+
+
 const socketIo = require('socket.io');
 const io = socketIo(server);
 
---
+++ test/server-test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const app = require('../server');
+const request = require('request');
+
+describe('Server', () => {
+
+  before(done => {
+    this.port = 9876;
+
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
+
+  it('should exist', () => {
+    assert(app);
+  });
+
+
+
+
+});
