1da11e7ac978d41fd2afbf5400f55f9655bb7d68
Brant Wellman <BrantWellman@gmail.com>
2016-04-21 12:58:48 -0600

    Add base form functionality
    Form to index page pulls in polling options
    Basic object created in server that sets values to 0 for each option
    Redirects to blank admin page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const generateId = require('./lib/generate-id');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,18 +9,36 @@ const server = http.createServer(app)
                  .listen(port, function () {
                   console.log('Listenting on port ' + port + '.');
                  });
+const socketIo = require('socket.io');
+const io = socketIo(server);
 
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.locals.poll = {};
+
 app.get('/', function (req, res){
   res.render('index');
 });
 
+app.get('/admin', function (req, res) {
+  res.render('admin');
+});
 
-const socketIo = require('socket.io');
-const io = socketIo(server);
+app.post('/surveys', (request, res) => {
+  // var surveyId = generateId();
+  var pollData = request.body.poll;
+  var poll = {};
+
+  pollData.responses.forEach(function(response){
+    poll[response] = 0;
+  });
+
+
+  console.log(poll);
+  res.redirect('/admin.html');
+});
 
 module.exports = server;
