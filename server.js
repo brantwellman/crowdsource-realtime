5fabd3646f5b7fb59224b8f8841e8fe7b8d2473d
Brant Wellman <BrantWellman@gmail.com>
2016-04-21 15:37:38 -0600

    Add admin link page
    Admin view with links displayed following survey submission
    Link to admin/survey view
    Link to survey view
    Survey class with question, responses, and id added

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+"use strict";
+
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const generateId = require('./lib/generate-id');
+const Survey = require('./lib/survey');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -28,18 +31,26 @@ app.get('/admin', function (req, res) {
   res.render('admin');
 });
 
+// app.get('/surveys/:id', (req, res) {
+//   app.locals.surveys
+//   res.render('survey, ')
+// });
+
 app.post('/surveys', (request, res) => {
-  // var surveyId = generateId();
+  var pollId = generateId();
+  // console.log(surveyId)
   var pollData = request.body.poll;
-  var poll = {};
+  var pollQuestion = pollData.question;
+  var pollResponses = {};
+  // console.log(pollData);
 
   pollData.responses.forEach(function(response){
-    poll[response] = 0;
+    pollResponses[response] = 0;
   });
 
-
-  console.log(poll);
-  res.render('admin');
+  var newSurvey = new Survey(pollId, pollQuestion, pollResponses);
+  // console.log(newSurvey.surveyId);
+  res.render('admin', { newSurvey: newSurvey });
 });
 
 module.exports = server;
